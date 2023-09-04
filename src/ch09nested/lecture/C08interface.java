d0ad6395bcf26cfe760532af3d5da8a2aa0120fa
Won A Young <zeronediary@naver.com>
2023-09-04 17:43:14 +0900

    중첩인터페이스

--- src/ch09nested/lecture/C08interface.java
+++ src/ch09nested/lecture/C08interface.java
@@ -6,10 +6,14 @@ public class C08interface {
             @Override
             public void method2() {
                 System.out.println("ConcreteClass.method2");
+                System.out.println("나는 메인메소드에 포함");
             }
         }
         ConcreteClass c = new ConcreteClass();
         c.method2();
+
+        MyClass08 myClass08 = new MyClass08();
+        myClass08.method1();
     }
 }
 
@@ -22,6 +26,7 @@ class MyClass081 implements MyClass08.NestedInterface {
 
 class MyClass08 {
     interface NestedInterface {
+        //중첩인터페이스에 올 수 있는 것들
         // public final static field
         // public abstract instance method (***********)
         void method2();
@@ -33,10 +38,10 @@ class MyClass08 {
 
     void method1() {
         class ConcreteClass implements NestedInterface {
-
             @Override
             public void method2() {
                 System.out.println("ConcreteClass.method2");
+                System.out.println("나는 method01에 포함");
             }
         }
 
